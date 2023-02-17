0ca35ce6feada464dd0fd31de66ee6135153012e
thelogicguy <107140254+thelogicguy@users.noreply.github.com>
2023-02-17 08:49:14 -0800

    Print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - A program that prints alphabets in lower
+ * 	case followed by a new line.
+ *
+ * Return: Always 0.
+*/
+int main(void)
+{
+	char alphabet;
+
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+		putchar(alphabet);
+
+	putchar('\n');
+
+	return (0);
+
+}
