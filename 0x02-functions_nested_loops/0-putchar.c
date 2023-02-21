1589fbbcb17913da45ec3f0084fb9103a8133fd9
thelogicguy <107140254+thelogicguy@users.noreply.github.com>
2023-02-21 10:08:11 -0800

    Print _putchar followed by a newline

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -5,11 +5,7 @@
 #include "main.h"
 
 /**
- * main: Entry point
- *
- *
- * Description: Prints _putchar followed by a new line.
- *
+ * main - Prints "_putchar" followed by a new line.
  *
  * Return: Always 0 (Sucess)
  */
